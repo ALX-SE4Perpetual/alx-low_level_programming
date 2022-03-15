9aaef3342765728d500b0643e1a8340752818913
Perpetual <perpetualmeninwa@gmail.com>
2022-03-15 13:54:06 +0000

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
 */
 int main(void)
 {
-char q[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
 char q[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
 write(2, q, sizeof(q));
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
