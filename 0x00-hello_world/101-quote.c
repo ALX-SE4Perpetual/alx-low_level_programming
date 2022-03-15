15556a266f9587241bc31f41e92f0c230b570b3a
Perpetual <perpetualmeninwa@gmail.com>
2022-03-15 13:38:35 +0000

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry point
+* Return: 1
+*/
+int main(void)
+{
+char q[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+char q[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+write(2, q, sizeof(q));
+return (1);
+}
