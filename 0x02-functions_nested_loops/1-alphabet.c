f3c8d919c9b4223ce49c1d584887c2ff6aee27e7
Perpetual <perpetualmeninwa@gmail.com>
2022-03-20 16:47:06 +0100

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet
+ * description: prints alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
