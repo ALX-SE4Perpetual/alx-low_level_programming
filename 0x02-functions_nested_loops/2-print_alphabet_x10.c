596fb3e018df8acec05df1e0ecce0930a7c07320
Perpetual <perpetualmeninwa@gmail.com>
2022-03-20 17:00:30 +0100

    10X Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - prints alphabets 10x
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int times;
+
+	for (times = 0; times <= 9; times++)
+	{
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+	putchar(alpha);
+	}
+	}
+}
+
