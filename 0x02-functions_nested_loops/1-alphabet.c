56db150130b11f4e8233c614d2d13c3bf3253372
Perpetual <perpetualmeninwa@gmail.com>
2022-03-20 17:14:15 +0100

    10X alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * print_alphabet - prints alphabet
  * description: prints alphabet
  * Return: 0
  */
+
 void print_alphabet(void)
 {
 	int i = 'a';
 
 	while (i <= 'z')
 	{
-		_putchar(i);
+		putchar(i);
 		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
