c5f3cc29ebbfa8b43cd3fef898d64ac0cd472c7d
Perpetual <perpetualmeninwa@gmail.com>
2022-03-26 16:18:15 +0100

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  *
  * Description: prints Dor's quote
  *
- *Return: Always 0 (Success)
+ *Return: 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59;
+write (2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
