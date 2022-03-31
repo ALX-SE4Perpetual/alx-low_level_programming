0508149fb08e6ce29364773145a234a6f1c61294
Perpetual <perpetualmeninwa@gmail.com>
2022-03-31 09:49:14 +0100

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,19 +3,19 @@
 /**
  * _puts_recursion - prints a string, followed by a new linevoid _puts_recursion(char *s)
  * @s: string
- * Return: pointer
+ * Return: void
  */
 
 void _puts_recursion(char *s)
 
 {
-if (*s != '\0')
-{
-_putchar(*(s);
-_puts_recursion((s + 1));
-}
-else
+if (*s == '\0')
 {
 _putchar('\n');
+return;
 }
+
+_putchar (*s);
+_puts_recursion(s + 1);
+
 }
