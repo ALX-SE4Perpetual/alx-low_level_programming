dae12496518c8a0fd349fb99d6634ccef909c3e6
Perpy-del <perpetualmeninwa@gmail.com>
2022-03-31 09:19:38 -0700

    Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *_evaluate - evaluate function sqrt
  *@i: interger
@@ -21,24 +22,4 @@ return (i);
 return (-1);
 
 return (-1);
-}
-
-/**
- *_sqrt_recursion - evaluate sqrt
- *@n: interger
- *Return: Sgrt_recursion
-*/
-
-int _sqrt_recursion(int n)
-{
-int i = 0;
-
-if (i < 0) /*If n is negative*/
-return (-1);
-
-else
-{
-return (_evaluate(i, n)); /*Recursive call*/
-}
-
 }
