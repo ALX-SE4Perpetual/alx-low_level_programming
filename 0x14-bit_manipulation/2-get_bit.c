b050555a8cde361cafebeeea9df68522ad1beac2
Perpetual <perpetualmeninwa@gmail.com>
2022-05-03 02:45:47 +0000

    Get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,8 +12,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
+
 	if (n == 0 && index < 64)
 		return (0);
+
 
 	for (i = 0; i <= 63; n >>= 1, i++)
 	{
@@ -22,6 +24,7 @@ int get_bit(unsigned long int n, unsigned int index)
 			return (n & 1);
 		}
 	}
+
 
 	return (-1);
 }
